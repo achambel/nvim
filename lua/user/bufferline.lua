a5c4a3de2f2e2ed12f9fc7813db6c9b883183287
Adriano <acmlima.softweb@gmail.com>
2023-05-25 19:39:29 +0100

    fix: bufferline colorscheme

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -56,6 +56,7 @@ bufferline.setup {
     --   end
     -- end,
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+    color_icons = true,
     show_buffer_icons = true,
     show_buffer_close_icons = true,
     show_close_icon = true,
@@ -63,7 +64,6 @@ bufferline.setup {
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
-    separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
     enforce_regular_tabs = true,
     always_show_bufferline = true,
     -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
