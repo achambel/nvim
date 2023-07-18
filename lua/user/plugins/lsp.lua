90c1f6bb25d4575216516f79452d7924f0018c17
Adriano <acmlima.softweb@gmail.com>
2023-07-18 13:37:15 +0100

    feat: add prettier to lsp

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -52,7 +52,7 @@ return {
           null_ls.builtins.diagnostics.credo.with({
             filetypes = { "elixir" }
           }),
-          null_ls.builtins.code_actions.gitsigns
+          null_ls.builtins.formatting.prettier
         }
       })
     end)
