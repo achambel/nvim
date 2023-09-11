85d50121cd554b617ac9546b946075daae154311
Adriano <acmlima.softweb@gmail.com>
2023-09-11 17:55:52 +0100

    add lexical for elixir language server

--- lua/user/config/lsp/servers/elixirls.lua
+++ lua/user/config/lsp/servers/elixirls.lua
@@ -1,4 +1,5 @@
 return {
-  cmd = { "elixir-ls" },
-  fetchDeps = false,
+  filetypes = { "elixir", "eelixir", "heex", "surface" },
+  cmd = { vim.fn.expand("~/coding/elixir/lexical/_build/dev/package/lexical/bin/start_lexical.sh") },
+  settings = {},
 }
